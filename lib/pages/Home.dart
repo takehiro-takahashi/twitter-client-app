34cbb63e90f851c9b7f40267ef2a4cbe6a20708a
Takehiro Takahashi <taka.mc.0509@gmail.com>
2018-10-09 14:58:34 +0900

    timeline add

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -10,7 +10,28 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Home"),
+        child: ListView(
+          children: List.generate(11, (index){
+            return InkWell(
+              onTap: (){},
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text("Index ユーザー $index"),
+                    leading: Image.asset(
+                      "assets/icon$index.png",
+                      width: 30.0,
+                      height: 30.0,
+                    ),
+                    subtitle: Text("こんにちはTwitterへ！！\nこちらはTwitterのクローンを作成しています"),
+                  ),
+                  SizedBox(height: 20.0,),
+                  Divider(height: 5.0,),
+                ],
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
