9bc70fbc3361dbe51c3fbdd1744ddd96d89342c3
TakehiroTakahashi <taka.mc.0509@gmail.com>
2018-10-26 22:23:53 +0900

    add message view

--- lib/pages/Message.dart
+++ lib/pages/Message.dart
@@ -9,9 +9,34 @@ class _MessageState extends State<Message> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Message"),
-      ),
+      body: ListView(
+        children: List.generate(10, (index) {
+          return Container(
+            margin: EdgeInsets.all(10.0),
+            child: ListTile(
+              leading: Image.asset(
+                "assets/icon$index.png",
+                width: 50.0,
+                height: 50.0,
+              ),
+              title: Row(
+                children: <Widget>[
+                  Text("Index $index さん"),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Text("@Index_$index"),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Text("$index時間前"),
+                ],
+              ),
+              subtitle: Text("ダイレクトメッセージを送信しています\n確認ご返事をしてください"),
+            ),
+          );
+        }),
+      )
     );
   }
 }
